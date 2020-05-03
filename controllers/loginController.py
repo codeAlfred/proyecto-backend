7061e3b387cd58032a5badcf84854ab3ba232072
codeAlfred <munozrodas.luisalfredo@gmail.com>
2020-05-03 01:17:55 -0500

    validar el ingreso en base al correo y password y cambiar el estado del usuario a Conectado al momento de logearse

--- controllers/loginController.py
+++ controllers/loginController.py
@@ -10,13 +10,25 @@ class LoginController(Resource):
 
   def post(self):
     data = request.get_json()
+    # obtener el email y password enviado en el JSON
     email = data.get("email")
     password = data.get("password")
-
-    authorized = True if password =="12345" else False
+    # buscar al usuario por medio de su email (el email es unico)
+    user= User.query.filter_by(email=email).first()
+    # formatear el usuario a un diccionario
+    user1=userSchema.dump(user)
+    # validar si el email y password son iguales
+    authorized = True if user1['password'] == password and user1['email'] == email else False
+    # authorized = True if password =="12345" else False
 
     if not authorized:
       return {'error':'Email or password invalid'},401
+    else:
+      # cambiar el estado a conectado
+      idEstado=1
+      user.estado_id = idEstado
+
+      db.session.commit()
 
     expires = datetime.timedelta(days=8)
     access_token = create_access_token(identity=str(1), expires_delta=expires)
