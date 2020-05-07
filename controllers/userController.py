c88ad3f5570c09ddc8cfca9f6e8ebddfbad899f5
codeAlfred <munozrodas.luisalfredo@gmail.com>
2020-05-07 01:09:44 -0500

    refactorizando el codigo para actualizar usuario y el metodo para validar el movil

--- controllers/userController.py
+++ controllers/userController.py
@@ -1,11 +1,13 @@
 from flask import request
 from flask_jwt_extended import jwt_required#jwt
 
+
 #Modulos propios del sistema
 from models import *
 from flask_restx import Resource
 
 import datetime
+import re
 
 class UserController(Resource):
   # BUSCAR un usuario por su id
@@ -15,23 +17,25 @@ class UserController(Resource):
     return userSchema.dump(user)
 
   # ACTUALIZAR un usuario por su id
-  @jwt_required
+  # @jwt_required
   def put(self, id):
 
     data = request.get_json()
-    date_time_str = data['fechaNacimiento']
-
-    date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d')
-
+    
     user = User.query.filter_by(id=id).first()
     if "nombre" in data: user.nombre = data["nombre"]
     if "apellido" in data: user.apellido = data["apellido"]
     if "password" in data: user.password = data["password"]
     if "email" in data: user.email = data["email"]
     if "movil" in data: user.movil = data["movil"]
-    if "fechaNacimiento" in data: user.fechaNacimiento = date_time_obj
+    if "fechaNacimiento" in data: 
+      date_time_obj = datetime.datetime.strptime(data['fechaNacimiento'], '%Y-%m-%d')
+      user.fechaNacimiento = date_time_obj
     if "foto" in data: user.foto = data["foto"]
     if "description" in data: user.description = data["description"]
+    if "estado_id" in data: user.estado_id= data['estado_id'],
+    if "sede_id" in data: user.sede_id = data['sede_id'],
+    if "especialidad_id" in data: user.especialidad_id = data['especialidad_id']
     db.session.commit()
 
     user = User.query.filter_by(id=id).first()
@@ -58,15 +62,11 @@ class UserPostController(Resource):
   def post(self):
     data = request.get_json()
 
-    # convertir la fecha de tipo string a tipo datetime
-    date_time_obj = datetime.datetime.strptime(data['fechaNacimiento'], '%Y-%m-%d')
+    if validarMovil(data["movil"]).get("valor"):
+       # convertir la fecha de tipo string a tipo datetime
+      date_time_obj = datetime.datetime.strptime(data['fechaNacimiento'], '%Y-%m-%d')
 
-    telefono = data["movil"]
-
-    if len(telefono) == 12:
-      if telefono[:4] == "+519":
-        if telefono[4:].isnumeric():
-          new_user = User(
+      new_user = User(
               nombre=data['nombre'],
               apellido=data['apellido'],
               password = data['password'],
@@ -80,15 +80,12 @@ class UserPostController(Resource):
               especialidad_id = data['especialidad_id']
             )
 
-          db.session.add(new_user)
-          db.session.commit()
-          return userSchema.dump(new_user)
-        else:
-          return {'error':'ingresar solo numeros del 0 al 9'},400
-      else:
-        return {'error':'el codigo postal es +51 y el movil debe comenzar con 9'},400
+      db.session.add(new_user)
+      db.session.commit()
+      return userSchema.dump(new_user)
     else:
-      return {'error':'tamaño demasiado grande para el movil'},400
+      return validarMovil(data["movil"]).get("mensaje")
+
 
 
 
@@ -137,6 +134,7 @@ class UserStateController(Resource):
 
       return usersSchema.dump(users)
     else:
+      
       return {'error':'estado enviado no se encuentra en la base de datos'},400
 
   # ACTUALIZAR o cambiar el estado de un usuario y guarda la fecha y hora del ultimo estado de conectado
@@ -218,3 +216,14 @@ def validarIdEspecialidad(idEspecialidad):
     return False
 
 
+def validarMovil(movil):
+    if len(movil) == 12:
+      if movil[:4] == "+519":
+        if movil[4:].isnumeric():
+          return {"valor":True, "mensaje":"movil correcto"}
+        else:
+          return {"valor":False, 'mensaje':'ingresar solo numeros del 0 al 9'}
+      else:
+        return {"valor":False, 'mensaje':'el codigo postal es +51 y el movil debe comenzar con 9'}
+    else:
+      return {"valor":False, 'mensaje':'tamaño demasiado grande para el movil'}
